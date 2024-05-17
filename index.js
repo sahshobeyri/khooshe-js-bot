7eca32a2cd3b1f2d903ef12c5acab7383c925969
sahshobeyri <sahshobeyri@gmail.com>
2024-05-17 22:02:00 +0330

    add index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log("HELLLLLO")
+
+const TeleBot = require('telebot');
+const bot = new TeleBot("5163059849:AAEbL-jSd79atSO996RGVIZlm3XP_IZC6mQ");
+
+bot.on('text', (msg) => msg.reply.text(msg.text));
+
+bot.start();
