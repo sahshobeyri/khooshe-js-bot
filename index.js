6548ed5d76058f522abc92fca8639c830a03f35c
sahshobeyri <sahshobeyri@gmail.com>
2024-05-18 02:06:39 +0330

    new TOKEN

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 console.log("HELLLLLO")
 
 const TeleBot = require('telebot');
-const bot = new TeleBot("5163059849:AAE6Mm-ob0BqB1QxQIZZxFeMYRo5Mn9-zPg");
+const bot = new TeleBot("1209877287:AAHibo60qhKBqWaJVq6A-CzB4hDeSvMCGls");
 
 bot.on('/start', (msg) => msg.reply.text('Welcome!'));
 bot.on('text', (msg) => msg.reply.text("Hello " + msg.text));
