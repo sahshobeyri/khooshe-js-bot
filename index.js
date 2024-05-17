0c00927daccaa3006e791b7759a448d4c12c3305
sahshobeyri <sahshobeyri@gmail.com>
2024-05-17 22:49:05 +0330

    welcome on /start

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ console.log("HELLLLLO")
 const TeleBot = require('telebot');
 const bot = new TeleBot("5163059849:AAFb7D1wmX9LJicdJ6nPUfQ0O3WMjZIxffY");
 
-bot.on('text', (msg) => msg.reply.text("Hello" + msg.text));
+bot.on('/start', (msg) => msg.reply.text('Welcome!'));
+bot.on('text', (msg) => msg.reply.text("Hello " + msg.text));
 
 bot.start();
