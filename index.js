3756de412b90b3ba043ac931735e68eb32f896f9
sahshobeyri <sahshobeyri@gmail.com>
2024-05-17 22:35:30 +0330

    change FUCKINGGGG TOKEN

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 console.log("HELLLLLO")
 
 const TeleBot = require('telebot');
-const bot = new TeleBot("5163059849:AAEbL-jSd79atSO996RGVIZlm3XP_IZC6mQ");
+const bot = new TeleBot("5163059849:AAFb7D1wmX9LJicdJ6nPUfQ0O3WMjZIxffY");
 
 bot.on('text', (msg) => msg.reply.text("Hello" + msg.text));
 
