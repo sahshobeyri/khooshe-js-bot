355f371aef7eb458f60248303c3a5926f9fd4873
sahshobeyri <sahshobeyri@gmail.com>
2024-05-18 02:09:37 +0330

    minor change

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const TeleBot = require('telebot');
 const bot = new TeleBot("1209877287:AAHibo60qhKBqWaJVq6A-CzB4hDeSvMCGls");
 
 bot.on('/start', (msg) => msg.reply.text('Welcome!'));
-bot.on('text', (msg) => msg.reply.text("Hello " + msg.text));
+bot.on('text', (msg) => msg.reply.text("Hello, so you said " + msg.text));
 
 bot.start();
