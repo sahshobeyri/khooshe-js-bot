b919327b0e73e83b587f165d46d6a5b2211eb90d
sahshobeyri <sahshobeyri@gmail.com>
2024-05-18 02:33:04 +0330

    add dotenv package

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 console.log("HELLLLLO")
 
 const TeleBot = require('telebot');
-const bot = new TeleBot("1209877287:AAHibo60qhKBqWaJVq6A-CzB4hDeSvMCGls");
+const bot = new TeleBot(process.env.BOT_TOKEN);
 
 bot.on('/start', (msg) => msg.reply.text('Welcome!'));
 bot.on('text', (msg) => msg.reply.text("Hello, so you said " + msg.text));
