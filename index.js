fa8208550993338f7c33d2b842357d5956e92cb6
sahshobeyri <sahshobeyri@gmail.com>
2024-05-18 05:48:41 +0330

    minor fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ console.log("HELLLLLO")
 require('dotenv')
 
 const { Telegraf } = require('telegraf')
-// const { message } = require('telegraf/filters')
+const { message } = require('telegraf/filters')
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 // bot.start((ctx) => ctx.reply('Welcome'))
