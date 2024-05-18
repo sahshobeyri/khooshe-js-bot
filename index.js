1d90de3c393cbc0f4e4f6776b9cf65f63c777da1
sahshobeyri <sahshobeyri@gmail.com>
2024-05-18 05:35:33 +0330

    revert to telegraf glass btn

--- index.js
+++ index.js
@@ -11,38 +11,19 @@ const bot = new Telegraf(process.env.BOT_TOKEN)
 // bot.hears('hi', (ctx) => ctx.reply('Hey there'))
 // bot.launch().then()
 
-// bot.start((ctx) => ctx.reply('سلام! لطفا یکی از گزینه‌ها را انتخاب کنید.', {
-//   reply_markup: {
-//     inline_keyboard: [
-//       [
-//         { text: 'گزینه 1', callback_data: 'option1' },
-//         { text: 'گزینه 2', callback_data: 'option2' }
-//       ]
-//     ]
-//   }
-// }));
-//
-// bot.action('option1', (ctx) => ctx.reply('شما گزینه 1 را انتخاب کردید.'));
-// bot.action('option2', (ctx) => ctx.reply('شما گزینه 2 را انتخاب کردید.'));
+bot.start((ctx) => ctx.reply('سلام! لطفا یکی از گزینه‌ها را انتخاب کنید.', {
+  reply_markup: {
+    inline_keyboard: [
+      [
+        { text: 'گزینه 1', callback_data: 'option1' },
+        { text: 'گزینه 2', callback_data: 'option2' }
+      ]
+    ]
+  }
+}));
 
-bot.start((ctx) => {
-  return ctx.reply('لطفا گزینه مورد نظر خود را انتخاب کنید', Markup.keyboard([
-    ['کباب', 'پیتزا']
-  ]).oneTime()
-    .resize()
-    .extra());
-});
-
-bot.hears('کباب', (ctx) => {
-  ctx.reply('شما گزینه کباب را انتخاب کردید.');
-  ctx.reply('لطفا گزینه مورد نظر خود را انتخاب کنید', Markup.removeKeyboard().extra());
-});
-
-bot.hears('پیتزا', (ctx) => {
-  ctx.reply('شما گزینه پیتزا را انتخاب کردید.');
-  ctx.reply('لطفا گزینه مورد نظر خود را انتخاب کنید', Markup.removeKeyboard().extra());
-});
-
+bot.action('option1', (ctx) => ctx.reply('شما گزینه 1 را انتخاب کردید.'));
+bot.action('option2', (ctx) => ctx.reply('شما گزینه 2 را انتخاب کردید.'));
 
 bot.launch().then();
 
