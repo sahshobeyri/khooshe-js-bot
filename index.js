7e81b5aa65be9fb1e0be8675583d4a229747ea90
sahshobeyri <sahshobeyri@gmail.com>
2024-05-18 19:41:08 +0330

    better showing LESSONS

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const LESSONS = [
 function generate_lessons_list(){
   let result = ""
   for (const idx in LESSONS) {
-    result += `${String(idx + 1)}. ${LESSONS[idx].title}` + "\n"
+    result += `${parseInt(idx) + 1}. ${LESSONS[idx].title}` + "\n"
   }
   return result
 }
