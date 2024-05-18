697c3cf3e7bb51c26c3628ef7e29f3647d526e68
sahshobeyri <sahshobeyri@gmail.com>
2024-05-18 03:44:56 +0330

    add show kitty

--- index.js
+++ index.js
@@ -7,4 +7,8 @@ const bot = new TeleBot(process.env.BOT_TOKEN);
 bot.on('/start', (msg) => msg.reply.text('Welcome!'));
 bot.on('text', (msg) => msg.reply.text("Hello, so you said " + msg.text));
 
+bot.on(/(show\s)?kitty*/, (msg) => {
+  return msg.reply.photo('http://thecatapi.com/api/images/get');
+});
+
 bot.start();
