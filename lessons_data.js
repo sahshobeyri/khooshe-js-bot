daa63d608e0dc4446779d783edbce87bb369c62a
sahshobeyri <sahshobeyri@gmail.com>
2024-05-26 03:04:56 +0330

    add quiz to lessons_data.js

--- lessons_data.js
+++ lessons_data.js
@@ -3,30 +3,30 @@ module.exports = [
     id:0,
     title: "مکانیک کوانتوم",
     length: 3,
-    frames: [
-      "img/lessons/0/0",
-      "img/lessons/0/1",
-      "img/lessons/0/2",
-    ]
+    quiz: {
+      question: 'مکانیک کوانتوم در مورد کدام جهان است؟',
+      options:['جهان بسیار کوچک','جهان بسیار بزرگ'],
+      correct: 0,
+    }
   },
   {
     id:1,
     title: "اصول رهبری و مذاکره",
     length: 3,
-    frames: [
-      "img/lessons/1/0",
-      "img/lessons/1/1",
-      "img/lessons/1/2",
-    ]
+    quiz: {
+      question: 'یک رهبر خوب باید...',
+      options:['کاریزماتیک باشد.','مقتدر باشد.'],
+      correct: 0,
+    }
   },
   {
     id:2,
     title: "نحو عربی",
     length: 3,
-    frames: [
-      "img/lessons/2/0",
-      "img/lessons/2/1",
-      "img/lessons/2/2",
-    ]
+    quiz: {
+      question: 'فاعل در جمله فعلیه، چه اعرابی میگیرد؟',
+      options:['نصب','رفع'],
+      correct: 1,
+    }
   },
 ];
