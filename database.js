b36099ea895bf30ce486c856a637ed2e5cb2ebac
sahshobeyri <sahshobeyri@gmail.com>
2024-05-27 04:58:10 +0330

    new style of accessing in database.js

--- database.js
+++ database.js
@@ -1,22 +1,54 @@
-const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
+//
+// // const url = 'mongodb://localhost:27017';
+// const url = 'mongodb://khooshe-bot-db.sahshobeyri.svc:27017';
+// // const url = 'khooshe-bot-db.sahshobeyri.svc:27017';
+// const dbName = 'KhoosheDB';
+//
+// let db;
+//
+// console.log("I AM DB CODE!")
+//
+// MongoClient.connect(url, (err, client) => {
+//   if (err) {
+//     console.log("EROROE DB")
+//     console.error(err);
+//   } else {
+//     console.log('Connected to MongoDB');
+//     db = client.db(dbName);
+//   }
+// }).then();
+//
+// module.exports = db;
 
-// const url = 'mongodb://localhost:27017';
-const url = 'mongodb://khooshe-bot-db.sahshobeyri.svc:27017';
-// const url = 'khooshe-bot-db.sahshobeyri.svc:27017';
-const dbName = 'KhoosheDB';
+const { MongoClient } = require("mongodb");
+require('dotenv')
 
-let db;
+const {DB_PASS} = process.env;
 
-console.log("I AM DB CODE!")
+// Replace the uri string with your connection string.
+const url = "khooshe-bot-db.sahshobeyri.svc:27017"
+const uri = `mongodb+srv://root:${DB_PASS}@${url}?retryWrites=true&writeConcern=majority`;
 
-MongoClient.connect(url, (err, client) => {
-  if (err) {
-    console.log("EROROE DB")
-    console.error(err);
-  } else {
-    console.log('Connected to MongoDB');
-    db = client.db(dbName);
+const client = new MongoClient(uri);
+console.log("CREATED new MongoClient(uri)")
+
+async function run() {
+  try {
+    await client.connect();
+
+    const database = client.db('db');
+    console.log(database)
+    // const movies = database.collection('movies');
+
+    // Query for a movie that has the title 'Back to the Future'
+    // const query = { title: 'Back to the Future' };
+    // const movie = await movies.findOne(query);
+
+    // console.log(movie);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
   }
-}).then();
-
-module.exports = db;
+}
+run().catch(console.dir);
